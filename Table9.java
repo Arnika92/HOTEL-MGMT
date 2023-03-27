18668dda8b2573b7dec07138a1a6145733da35f3
Arnika92 <128769521+Arnika92@users.noreply.github.com>
2023-03-27 15:20:34 +0530

    java file

---
+++ Table9.java
@@ -0,0 +1,20 @@
+package day4;
+
+public class Table9 {
+
+	public static void main(String[] args) {
+	/*	int j=9;
+		for(int i=1;i<=10; i++ )
+		{
+			System.out.println(9*i);
+		}*/
+		for(int i =1; i<=10;i++)
+		{
+			int x=9;
+				System.out.println(++x);
+		}
+			
+			
+	}
+
+}
