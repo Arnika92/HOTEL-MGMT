b88472f4fee606cbb8654e6e8be52a6e2bee079a
Arnika92 <128769521+Arnika92@users.noreply.github.com>
2023-03-28 16:16:18 +0530

    JAVVAA

---
+++ Print.java
@@ -0,0 +1,20 @@
+package day5;
+
+public class Print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       char a='A';
+       for(int i=1;i<=5;i++)
+       { a='A';
+    	   for(int j=1; j<=i;j++)
+    	   {
+    		   System.out.print(a);
+    		   a++;
+    		  
+    	   }
+    	   System.out.println();
+       }
+	}
+
+}
